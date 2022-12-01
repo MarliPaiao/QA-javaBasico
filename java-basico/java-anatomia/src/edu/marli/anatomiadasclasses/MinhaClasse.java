d0e43341aad54bc137dfd73102bb62cf134f6ea9
MarliPaiao <andradepaiaomarli@gmail.com>
2022-12-01 15:29:57 -0300

    Conteúdo java para manter em pasta

---
+++ java-basico/java-anatomia/src/edu/marli/anatomiadasclasses/MinhaClasse.java
@@ -0,0 +1,11 @@
+package edu.marli.anatomiadasclasses;
+public class MinhaClasse {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+      
+        System.out.print("Java de novo");
+    }    
+}
